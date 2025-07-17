2cb86386e453fe53f98afc0d978ef011cc36456c
Januda Amaneth <jaariyasinghe2003@gmail.com>
2025-07-17 21:40:06 +0530

    feat: Implement Navbar Section and add images folder

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,41 @@
 import React from 'react';
 
 const Navbar = () => {
-    return <div>Navbar</div>;
+    return (
+        <header>
+            <a href="#home">
+                <img src="/images/logo.svg" alt="site-logo" />
+            </a>
+            <nav>
+                <div className="desktop-nav-links">
+                    <ul>
+                        <li>
+                            <a href="">Pricing</a>
+                        </li>
+                        <li>
+                            <a href="">Product</a>
+                        </li>
+                        <li>
+                            <a href="">About Us</a>
+                        </li>
+                        <li>
+                            <a href="">Careers</a>
+                        </li>
+                        <li>
+                            <a href="">Community</a>
+                        </li>
+                    </ul>
+                    <button className="get-started">Get Started</button>
+                </div>
+                <button className="menu-btn">
+                    <img
+                        src="/images/icon-hamburger.svg"
+                        alt="hamburger-icon"
+                    />
+                </button>
+            </nav>
+        </header>
+    );
 };
 
 export default Navbar;
