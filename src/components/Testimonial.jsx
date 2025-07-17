8f84cae12e9bae24514071ecc5e44e1b195850a8
Januda Amaneth <jaariyasinghe2003@gmail.com>
2025-07-17 22:03:19 +0530

    feat: Implement Testimonial Section

---
+++ src/components/Testimonial.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Testimonial = () => {
+    return (
+        <section>
+            <div>
+                <img src="/images/avatar-ali.png" alt="avatar" />
+                <h3>Ali Bravo</h3>
+                <p>
+                    “We have been able to cancel so many other subscriptions
+                    since using Manage. There is no more cross-channel confusion
+                    and everyone is much more focused.”
+                </p>
+            </div>
+            <button>Get Started</button>
+        </section>
+    );
+};
+
+export default Testimonial;
