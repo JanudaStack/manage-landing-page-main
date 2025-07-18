b4ad08590569c716eff9988ad04b14224fb983d0
Januda Amaneth <jaariyasinghe2003@gmail.com>
2025-07-18 23:43:07 +0530

    feat: Implement CSS to Navbar Section and Hero Section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,22 +3,30 @@ import React from 'react';
 const Hero = () => {
     return (
         <section>
-            <div className="visual-preferences">
-                <img
-                    src="/images/illustration-intro.svg"
-                    alt="illus-intro-img"
-                />
-                <img src="/images/bg-tablet-pattern.svg" alt="pattern-img" />
-            </div>
-            <div className="hero-content">
-                <h1 className="hero-header">
-                    Bring everyone together to build better products.
-                </h1>
-                <p className="hero-paragraph">
-                    Manage makes it simple for software teams to plan day-to-day
-                    tasks while keeping the larger team goals in view.
-                </p>
-                <button>Get Started</button>
+            <div className="visual-preferences grid grid-cols-1 justify-center lg:grid-cols-2">
+                <div>
+                    <img
+                        src="/images/illustration-intro.svg"
+                        alt="illus-intro-img"
+                        className="w-full"
+                    />
+                    <img
+                        src="/images/bg-tablet-pattern.svg"
+                        alt="pattern-img"
+                        className="absolute -z-10 top-0 left-0"
+                    />
+                </div>
+                <div className="hero-content">
+                    <h1 className="hero-header">
+                        Bring everyone together to build better products.
+                    </h1>
+                    <p className="hero-paragraph">
+                        Manage makes it simple for software teams to plan
+                        day-to-day tasks while keeping the larger team goals in
+                        view.
+                    </p>
+                    <button>Get Started</button>
+                </div>
             </div>
         </section>
     );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,32 +2,34 @@ import React from 'react';
 
 const Navbar = () => {
     return (
-        <header>
+        <header className="flex justify-between items-center py-4 px-3 z-50 w-full">
             <a href="#home">
                 <img src="/images/logo.svg" alt="site-logo" />
             </a>
-            <nav>
-                <div className="desktop-nav-links">
-                    <ul>
+            <nav className="lg:w-full">
+                <div className="desktop-nav-links hidden lg:flex">
+                    <ul className="flex items-center gap-5 mx-auto">
                         <li>
-                            <a href="">Pricing</a>
+                            <a href="#">Pricing</a>
                         </li>
                         <li>
-                            <a href="">Product</a>
+                            <a href="#">Product</a>
                         </li>
                         <li>
-                            <a href="">About Us</a>
+                            <a href="#">About Us</a>
                         </li>
                         <li>
-                            <a href="">Careers</a>
+                            <a href="#">Careers</a>
                         </li>
                         <li>
-                            <a href="">Community</a>
+                            <a href="#">Community</a>
                         </li>
                     </ul>
-                    <button className="get-started">Get Started</button>
+                    <button className="get-started bg-[var(--orange-400)] py-2 px-4 rounded-[2rem]">
+                        Get Started
+                    </button>
                 </div>
-                <button className="menu-btn">
+                <button className="menu-btn flex lg:hidden">
                     <img
                         src="/images/icon-hamburger.svg"
                         alt="hamburger-icon"
