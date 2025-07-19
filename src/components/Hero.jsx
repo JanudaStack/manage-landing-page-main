b9523c499d8b995a89ee3f2721ada2a6b6e2f8ad
Januda Amaneth <jaariyasinghe2003@gmail.com>
2025-07-19 10:32:24 +0530

    feat: Style Hero Component and Navbar Component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Hero = () => {
     return (
         <section>
-            <div className="visual-preferences grid grid-cols-1 justify-center lg:grid-cols-2">
+            <div className=" grid grid-cols-1 justify-center lg:grid-cols-2 lg:py-[5rem] lg:px-[8rem] overflow-x-hidden">
                 <div>
                     <img
                         src="/images/illustration-intro.svg"
@@ -13,19 +13,19 @@ const Hero = () => {
                     <img
                         src="/images/bg-tablet-pattern.svg"
                         alt="pattern-img"
-                        className="absolute -z-10 top-0 left-0"
+                        className="absolute -z-50 -top-50 -right-50"
                     />
                 </div>
-                <div className="hero-content">
-                    <h1 className="hero-header">
+                <div className="hero-content flex flex-col gap-10 lg:-order-1 lg:mr-25 px-4">
+                    <h1 className="hero-header text-[var(--blue-950)] text-2xl text-center lg:text-5xl font-bold lg:leading-14">
                         Bring everyone together to build better products.
                     </h1>
-                    <p className="hero-paragraph">
+                    <p className="hero-paragraph text-center text-slate-500 lg:text-lg lg:w-5/6">
                         Manage makes it simple for software teams to plan
                         day-to-day tasks while keeping the larger team goals in
                         view.
                     </p>
-                    <button>Get Started</button>
+                    <button className="btn">Get Started</button>
                 </div>
             </div>
         </section>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 const Navbar = () => {
     return (
-        <header className="flex justify-between items-center py-4 px-3 z-50 w-full">
+        <header>
             <a href="#home">
                 <img src="/images/logo.svg" alt="site-logo" />
             </a>
-            <nav className="lg:w-full">
+            <nav>
                 <div className="desktop-nav-links hidden lg:flex">
                     <ul className="flex items-center gap-5 mx-auto">
                         <li>
@@ -25,9 +25,7 @@ const Navbar = () => {
                             <a href="#">Community</a>
                         </li>
                     </ul>
-                    <button className="get-started bg-[var(--orange-400)] py-2 px-4 rounded-[2rem]">
-                        Get Started
-                    </button>
+                    <button className="btn">Get Started</button>
                 </div>
                 <button className="menu-btn flex lg:hidden">
                     <img
