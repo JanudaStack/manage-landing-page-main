92fa0a269a95d58505b8b5bfd898581c5dbdd628
Januda Amaneth <jaariyasinghe2003@gmail.com>
2025-07-21 22:00:00 +0530

    feat: Improve layout and styling of Testimonial Component for better responsiveness and user experience

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Testimonial = () => {
     return (
-        <section>
-            <div className="flex flex-col items-center">
+        <section className="flex flex-col justify-center my-30 gap-10">
+            <div className="flex flex-col items-center md:px-20 gap-10">
                 <h2 className="about-header text-2xl text-center">
                     What they've said
                 </h2>
@@ -13,13 +13,13 @@ const Testimonial = () => {
                     className="w-20"
                 />
                 <h3>Ali Bravo</h3>
-                <p className="px-10 text-center">
+                <p className="px-10 text-center md:w-4/8 md:text-xl">
                     “We have been able to cancel so many other subscriptions
                     since using Manage. There is no more cross-channel confusion
                     and everyone is much more focused.”
                 </p>
             </div>
-            <button className="btn">Get Started</button>
+            <button className="btn mx-auto">Get Started</button>
         </section>
     );
 };
