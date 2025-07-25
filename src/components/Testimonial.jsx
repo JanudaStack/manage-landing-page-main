c4e3280008ff6cb4a7db29db6cd3ce1d87ad1004
Januda Amaneth <jaariyasinghe2003@gmail.com>
2025-07-25 12:29:22 +0530

    feat: Update Testimonial component layout and styling for improved visual hierarchy; add new section comment in CSS

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -20,7 +20,7 @@ const Testimonial = () => {
     });
 
     return (
-        <section className="flex flex-col justify-center my-30 gap-10">
+        <section className="flex flex-col justify-center mt-20 mb-30 gap-10 ">
             <div className="flex flex-col items-center md:px-20 gap-10">
                 <h2 className="about-header text-2xl text-center">
                     What they've said
@@ -32,11 +32,18 @@ const Testimonial = () => {
                             slideIndex === index + 1 ? 'block' : 'hidden'
                         }`}
                     >
-                        <img src={client.img} alt="avatar" className="w-20" />
-                        <h3>{client.name}</h3>
-                        <p className="px-10 text-center md:w-4/8 md:text-xl">
-                            {client.review}
-                        </p>
+                        <img
+                            src={client.img}
+                            alt="avatar"
+                            className="w-20 relative z-[999] top-15"
+                        />
+                        <div className="bg-[var(--gray-50)] flex flex-col gap-4 justify-center items-center pb-10 pt-20 w-3/4 lg:w-2/3">
+                            <h3>{client.name}</h3>
+                            <p className="px-0 text-center md:w-4/8 md:text-xl">
+                                {client.review}
+                            </p>
+                        </div>
+
                         <div className="flex gap-2">
                             {testimonialData.map((_, index) => (
                                 <button
