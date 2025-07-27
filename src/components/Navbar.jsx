f2737d58a051a48e6ac10ac1e5d8b47ab06bedf1
Januda Amaneth <jaariyasinghe2003@gmail.com>
2025-07-27 13:12:16 +0530

    feat: Implement mobile navigation menu with toggle functionality and styling; enhance CSS for mobile layout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Navbar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
     return (
         <header>
             <a href="#home">
@@ -27,12 +32,38 @@ const Navbar = () => {
                     </ul>
                     <button className="btn">Get Started</button>
                 </div>
-                <button className="menu-btn">
+                <button className="menu-btn" onClick={toggleMenu}>
                     <img
                         src="/images/icon-hamburger.svg"
                         alt="hamburger-icon"
                     />
                 </button>
+                <div
+                    className={`mobile-nav-container ${
+                        menuOpen
+                            ? 'top-20 translate-y-0 opacity-100'
+                            : '-translate-y-full opacity-0'
+                    }`}
+                >
+                    <ul className="mobile-nav-links">
+                        <li>
+                            <a href="#">Pricing</a>
+                        </li>
+                        <li>
+                            <a href="#">Product</a>
+                        </li>
+                        <li>
+                            <a href="#">About Us</a>
+                        </li>
+                        <li>
+                            <a href="#">Careers</a>
+                        </li>
+                        <li>
+                            <a href="#">Community</a>
+                        </li>
+                        <button className="btn mt-5">Get Started</button>
+                    </ul>
+                </div>
             </nav>
         </header>
     );
